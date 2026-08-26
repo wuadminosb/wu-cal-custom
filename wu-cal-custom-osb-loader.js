cd2d45d2afbf70a51c8073733428057890c152e5
wuadminosb <manuel.toth@wu.ac.at>
2026-08-26 12:02:41 +0200

    feat: load event space room details

--- wu-cal-custom-osb-loader.js
+++ wu-cal-custom-osb-loader.js
@@ -3,11 +3,11 @@
 
     const MAIN_BRANCH = 'main';
     const CDN_BASE = 'https://cdn.jsdelivr.net/gh/wuadminosb/wu-cal-custom@' + MAIN_BRANCH + '/';
-
     const BASE_URL = CDN_BASE + 'wu-cal-custom.js';
     const SLIDER_URL = CDN_BASE + 'wu-cal-custom-slider.js?v=20260805-1';
     const ROOM_DETAILS_URL = CDN_BASE + 'wu-cal-custom-room-details.js?v=20260826-1';
     const ROOM_DETAILS_3_6_URL = CDN_BASE + 'wu-cal-custom-room-details-3-6.js?v=20260826-1';
+    const EVENT_SPACE_DETAILS_URL = CDN_BASE + 'wu-cal-custom-room-details-event-spaces-loader.js?v=20260826-1';
     const ACCOUNT_CHOICE_URL = CDN_BASE + 'wu-cal-custom-account-choice.js?v=20260805-1';
     const CALENDAR_TABS_URL = CDN_BASE + 'wu-cal-custom-calendar-tabs.js?v=20260805-1';
     const HEADER_CUSTOM_URL = CDN_BASE + 'wu-cal-custom-header.js?v=20260805-1';
@@ -87,6 +87,9 @@
         })
         .then(function () {
             return loadScript(ROOM_DETAILS_3_6_URL);
+        })
+        .then(function () {
+            return loadScript(EVENT_SPACE_DETAILS_URL);
         })
         .then(function () {
             return loadScript(ACCOUNT_CHOICE_URL);
