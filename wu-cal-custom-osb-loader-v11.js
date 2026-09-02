929b79c4607bdb1d7ca30c1112d1c350646d9310
wuadminosb <manuel.toth@wu.ac.at>
2026-09-02 10:34:45 +0200

    Load approved frameless event styling

--- wu-cal-custom-osb-loader-v11.js
+++ wu-cal-custom-osb-loader-v11.js
@@ -4,7 +4,7 @@
   const SELF = document.currentScript?.src || '';
   const match = SELF.match(/^(https:\/\/cdn\.jsdelivr\.net\/gh\/wuadminosb\/wu-cal-custom@[^/]+\/)/);
   const BASE = match ? match[1] : 'https://cdn.jsdelivr.net/gh/wuadminosb/wu-cal-custom@main/';
-  const VERSION = '20260902-1';
+  const VERSION = '20260902-2';
 
   function load(url) {
     return new Promise((resolve, reject) => {
@@ -29,6 +29,7 @@
   const finalUrl = BASE + 'wu-cal-custom-confirmation-same-tab-final.js?v=20260828-5';
   const myEventsUrl = BASE + 'wu-cal-custom-myevents-dashboard.js?v=' + VERSION;
   const viewEventUrl = BASE + 'wu-cal-custom-viewevent-dashboard.js?v=' + VERSION;
+  const framelessUrl = BASE + 'wu-cal-custom-events-frameless.js?v=' + VERSION;
 
   load(v8Url)
     .then(() => load(finalUrl))
@@ -36,6 +37,7 @@
       load(myEventsUrl),
       load(viewEventUrl)
     ]))
+    .then(() => load(framelessUrl))
     .then(() => {
       window.wuOsbLoaderVersion = VERSION;
       console.info('[WU OSB] Loader v11 aktiv:', VERSION, 'Base:', BASE);
